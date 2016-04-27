822b9d720af7ab9fa2a4b7dc9e0d2ca59405cf21
thanglyphan <lyern52@gmail.com>
2016-04-27 14:48:14 +0200

    Fix facebook login, done

--- app/controllers/facebookusers_controller.rb
+++ app/controllers/facebookusers_controller.rb
@@ -1,7 +1,7 @@
 class FacebookusersController < ApplicationController
   def create
     facebookuser = Facebookuser.from_omniauth(env["omniauth.auth"])
-    session[:facebookuser_id] = facebookuser.uid
+    session[:facebookuser_id] = facebookuser.id
     redirect_to root_path
   end
 
