622f8d6c70b4be6c5b55745f291a129b59a983fd
thanglyphan <lyern52@gmail.com>
2016-06-05 16:38:06 +0200

    Restrict calendar private

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -123,9 +123,14 @@ class SessionsController < ApplicationController
   end
 
   def send_me_mail
-    UserMailer.send_thang_email(@current_user, params[:InputName], params[:InputEmail], params[:subject], params[:InputMessage]).deliver_now
-    flash[:notice] = "OK"
-    redirect_to(:back)
+    if @current_user.present?
+      UserMailer.send_thang_email(@current_user, params[:InputName], params[:InputEmail], params[:subject], params[:InputMessage]).deliver_now
+      flash[:notice] = "OK"
+      redirect_to(:back)
+    else
+      UserMailer.send_thang_email(nil, params[:InputName], params[:InputEmail], params[:subject], params[:InputMessage]).deliver_now
+    end
+
   end
 
 
